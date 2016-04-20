8cd96d20db05b9fc166dd00ffb6e07d499a9d088
Sean McKenna <mckennapsean@gmail.com>
2016-04-20 14:55:24 -0600

    add a simple command line interface for running dijkstras

--- FindBestPathTester.java
+++ FindBestPathTester.java
@@ -1,5 +1,7 @@
 package assignment13;
 
+import java.util.Scanner;
+
 /**
  * <p>An example of how a user will use your best flight API.</p>
  * <p>You will still be required to writed JUnit tests to test your program.</p>
@@ -37,8 +39,63 @@ public class FindBestPathTester {
 //		BestPath cheapestPath = airportGraph.getBestPath("LAS", "LAX", FlightCriteria.COST);
 //		System.out.println(cheapestPath.toString());
 		
-		BestPath shortestDistancePath = airportGraph.getBestPath("IXS", "PWB", FlightCriteria.DISTANCE, "MQ");
-		System.out.println(shortestDistancePath.toString());
+		// BestPath shortestDistancePath = airportGraph.getBestPath("IXS", "PWB", FlightCriteria.DISTANCE, "MQ");
+		// System.out.println(shortestDistancePath.toString());
+		
+		// command-line user interface
+		if(args.length != 0){
+			System.out.println();
+			System.out.println("Please enter a search string for Dijkstra's:");
+			System.out.println("  ORIGIN DESTINATION CRITERIA CARRIER");
+			System.out.println("  e.g. SFO DFW DISTANCE DL");
+			System.out.println("Default criteria of DISTANCE and across all carriers.");
+			System.out.println("Input nothing to exit the interface.");
+			System.out.println();
+			
+			Scanner input = new Scanner(System.in);
+			String search = "SFO DFW DISTANCE DL";
+			while(search.length() != 0){
+				System.out.print("Dijkstra's: ");
+				
+				search = input.nextLine();
+				String[] searchArgs = search.split(" ");
+				BestPath shortestDistancePath;
+				String carrier = "";
+				
+				if(searchArgs.length > 1 && searchArgs.length < 5){
+					FlightCriteria criteria = FlightCriteria.DISTANCE;
+					if(searchArgs.length > 2){
+						switch(searchArgs[2]) {
+							case "COST":
+								criteria = FlightCriteria.COST;
+								break;
+							case "DELAY":
+								criteria = FlightCriteria.DELAY;
+								break;
+							case "DISTANCE":
+								criteria = FlightCriteria.DISTANCE;
+								break;
+							case "CANCELED":
+								criteria = FlightCriteria.CANCELED;
+								break;
+							case "TIME":
+								criteria = FlightCriteria.TIME;
+								break;
+							}
+					}
+					if(searchArgs.length > 3){
+						shortestDistancePath = airportGraph.getBestPath(searchArgs[0], searchArgs[1], criteria, searchArgs[3]);
+					}else{
+						shortestDistancePath = airportGraph.getBestPath(searchArgs[0], searchArgs[1], criteria);
+					}
+					System.out.println(shortestDistancePath);
+				}else{
+					System.out.println("Error - please utilize 2-4 search arguments.");
+				}
+				
+				System.out.println();
+			}
+		}
 	}
 
 }
