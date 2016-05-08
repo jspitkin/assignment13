52998773bcd80ba661bf1507679951ee292c21a4
Sean McKenna <mckennapsean@gmail.com>
2016-05-08 14:19:31 -0600

    have grading bash script write out to file

--- grading/grade.sh
+++ grading/grade.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 mkdir classes
 javac -d classes *.java
-java -cp .:classes:/Library/Java/Extensions autograderutils.JUnitPlugin assignment13.Assignment13GradingTests groups.properties extra.properties
+java -cp .:classes:/Library/Java/Extensions autograderutils.JUnitPlugin assignment13.Assignment13GradingTests groups.properties extra.properties > grader_output.rws
 rm -rf classes
