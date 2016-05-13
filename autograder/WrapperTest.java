f66f8c23bcb2729613e18ee21f278d2693f0e5db
rwsargent <rw.sarge@gmail.com>
2016-05-12 18:24:27 -0600

    Update WrapperTest.java

--- autograder/WrapperTest.java
+++ autograder/WrapperTest.java
@@ -9,7 +9,7 @@ public class WrapperTest {
 	public static void main(String[] args) {
 		String groupPath = new File("groups.properties").getAbsolutePath();
 		String extraPath = new File("extra.properties").getAbsolutePath();
-		String className = WrapperJunit.class.getName(); // or hardcode the string "package.ClassName"
-		JUnitWrapper.main(new String[] {className, groupPath, extraPath});
+		String className = Assignment13GradingTests.class.getName(); // or hardcode the string "package.ClassName"
+		JUnitPlugin.main(new String[] {className, groupPath, extraPath});
 	}
 }
